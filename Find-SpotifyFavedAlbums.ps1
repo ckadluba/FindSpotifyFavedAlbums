d3d050c2c44b3447088c5a69b42afa1c6efe0538
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2022-11-15 11:56:19 +0100

    More universal script
    * Parameters ReleaseDateBegin and ReleaseDateEnd, both optional, let the user specify closed and open ranges.
    * Optional parameter OnlyReleaseTypes lets the user specify a list of release types to filter for. If omitted all types are returned.
    * Return result as PSObjects allows further processing by caller.

--- Find-SpotifyFavedAlbums.ps1
+++ Find-SpotifyFavedAlbums.ps1
@@ -2,23 +2,20 @@
 param (
     [Parameter(Mandatory=$true)]
     [string]
-    $Year,
-
-    [Parameter(Mandatory=$true)]
-    [string]
     $AccessToken,
 
     [Parameter()]
-    [switch]
-    $OnlyAlbums,
+    [string]
+    $ReleaseDateBegin,
 
     [Parameter()]
-    [switch]
-    $PrintMetadata
-)
+    [string]
+    $ReleaseDateEnd,
 
-# Token can be generated with Spotify Dev console: https://spotify.dev/console/get-current-user-saved-albums/
-# Scope: user-library-read
+    [Parameter()]
+    [System.String[]]
+    $OnlyReleaseTypes
+)
 
 $authHeader = "Bearer " + $AccessToken
 $limit = 20
@@ -26,6 +23,7 @@ $offset = 0
 
 while ($true)
 {
+    # Query batch
     $uri = "https://api.spotify.com/v1/me/albums?limit=" + $limit + "&offset=" + $offset
     $response = Invoke-RestMethod -Uri $uri -Method GET -Headers @{Accept = "application/json"; Authorization = $authHeader}
     if (($null -eq $response) -or ($response.items.Count -eq 0))
@@ -33,25 +31,34 @@ while ($true)
         break
     }
 
-    if ($OnlyAlbums.IsPresent -eq $true)
+    # Filter result
+    $albumsOfYear = $response.items
+    if ($ReleaseDateBegin)
     {
-        $albumsOfYear = $response.items | Where-Object { $_.album.release_date.Contains($Year) -and ($_.album.album_type -eq "album" -or $_.album.album_type -eq "compilation") }
+        $albumsOfYear = $albumsOfYear | Where-Object { $_.album.release_date -ge $ReleaseDateBegin }
     }
-    else
+    if ($ReleaseDateEnd)
     {
-        $albumsOfYear = $response.items | Where-Object { $_.album.release_date.Contains($Year) }
+        $albumsOfYear = $albumsOfYear | Where-Object { $_.album.release_date -le $ReleaseDateEnd }
+    }
+    if ($null -ne $OnlyReleaseTypes)
+    {
+        $albumsOfYear = $albumsOfYear | Where-Object { $OnlyReleaseTypes.Contains($_.album.album_type) }
     }
 
+    # Output filtered batch as flattened objects
     foreach ($item in $albumsOfYear)
     {
-        $album = $item.album
-        $artist = $album.artists[0]
-        $line = "$($artist.name) - $($album.name) ($($album.label))"
-        if ($PrintMetadata.IsPresent -eq $true)
-        {
-            $line = "$line [released: $($album.release_date), type: $($album.album_type), tracks: $($album.tracks.total)]"
-        }
-        Write-Output $line
+        $album = New-Object PSObject
+
+        Add-Member -InputObject $album -MemberType NoteProperty -Name ArtistName -Value $item.album.artists[0].name
+        Add-Member -InputObject $album -MemberType NoteProperty -Name AlbumName -Value $item.album.name
+        Add-Member -InputObject $album -MemberType NoteProperty -Name Label -Value $item.album.label
+        Add-Member -InputObject $album -MemberType NoteProperty -Name ReleaseDate -Value $item.album.release_date
+        Add-Member -InputObject $album -MemberType NoteProperty -Name AlbumType -Value $item.album.album_type
+        Add-Member -InputObject $album -MemberType NoteProperty -Name NumberTracks -Value $item.album.tracks.total
+
+        Write-Output $album
     }
 
     $offset += $limit
